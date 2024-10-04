5ca30a91d471a74f6add5e66e71f3bdb6d69043c
kaya70875 <shortsforturkey@gmail.com>
2024-10-04 14:09:39 +0300

    create structure

--- manage-react/src/App.tsx
+++ manage-react/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import './index.css'
 
 function App() {
 
   return (
     <div className="app">
-      
+      <Navbar />
     </div>
   )
 }
